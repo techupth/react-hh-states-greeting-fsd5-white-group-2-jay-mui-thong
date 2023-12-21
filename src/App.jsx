347652b0afa3b599baa62ea6ab584f77350a4602
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2023-12-21 14:53:53 +0700

    Finish exercise hackhour

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingMessage, setgreetingMessage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => setgreetingMessage("สวัสดี")}>สวัสดี!</button>
+        <button onClick={() => setgreetingMessage("Hi!")}>Hi!</button>
+        <button onClick={() => setgreetingMessage("你好!")}>你好!</button>
       </div>
     </div>
   );
